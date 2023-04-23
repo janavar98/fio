832683b71380e258b8440cfdf5bc6be31fc4498b
janavar98 <131580628+janavar98@users.noreply.github.com>
2023-04-23 11:21:05 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 设置各变量
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
+UUID='25cfbf03-1c0e-41f2-8c34-be58bb24dd42'
 VMESS_WSPATH='/vmess'
 VLESS_WSPATH='/vless'
 TROJAN_WSPATH='/trojan'
